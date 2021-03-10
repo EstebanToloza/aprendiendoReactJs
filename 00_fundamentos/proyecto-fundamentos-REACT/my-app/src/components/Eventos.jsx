dc2136cc1ee5544f3e8a67a16051867588d328a7
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-03-10 15:24:29 -0300

    Cambiando div por Fragment

--- 00_fundamentos/proyecto-fundamentos-REACT/my-app/src/components/Eventos.jsx
+++ 00_fundamentos/proyecto-fundamentos-REACT/my-app/src/components/Eventos.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Eventos = () => {
     
@@ -7,11 +7,11 @@ const Eventos = () => {
     }
 
     return (
-        <div>
+        <Fragment> {/* Se usa Fragment cuando no es necesario utilizar un div. También se puede utilizar sólo etiquetas de apertura y cierre, sin llamar a Fragment */}
             <hr/>
             <h2>Eventos</h2>
             <button onClick= { () => eventoClick() } >Click</button>
-        </div>
+        </Fragment>
     )
 }
 
