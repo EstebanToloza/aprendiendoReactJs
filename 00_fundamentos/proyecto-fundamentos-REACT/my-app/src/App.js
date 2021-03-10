3b72cf1bb958a0b720f4c82b12601e9bab9b8daa
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-03-10 14:06:42 -0300

    Importando componentes

--- 00_fundamentos/proyecto-fundamentos-REACT/my-app/src/App.js
+++ 00_fundamentos/proyecto-fundamentos-REACT/my-app/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Parrafo from './components/Parrafo.jsx'
 
+//importando componente en react
 function App() {
   return (
     <div className="App">
       <h1>Hola mundo React</h1>
+      <Parrafo /> 
+      <Parrafo /> 
+      <Parrafo /> 
+      <Parrafo /> 
     </div>
   );
 }
---
+++ 00_fundamentos/proyecto-fundamentos-REACT/my-app/src/components/Parrafo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Parrafo = () => {
+    return (
+        <div>
+            <p>Mi primer componente en REACT!</p>
+        </div>
+    )
+}
+
+export default Parrafo
