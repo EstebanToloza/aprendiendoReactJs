daa6e17418987ff2d4e51a8d2cbf71ed2cd0b768
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-03-12 13:29:10 -0300

    Validando formulario

--- 00_fundamentos/proyecto-fundamentos-REACT/my-app/src/components/Formulario.jsx
+++ 00_fundamentos/proyecto-fundamentos-REACT/my-app/src/components/Formulario.jsx
@@ -5,10 +5,30 @@ const Formulario = () => {
     const [fruta, setFruta] = React.useState('')
     const [descripcion, setDescripcion] = React.useState('')
 
+    const guardarDatos = (e) =>{
+        e.preventDefault()
+        if(!fruta.trim()){    /* El método trim( ) devuelve la cadena de texto despojada de los espacios en blanco en ambos extremos. El método no afecta al valor de la cadena de texto. Mozilla*/
+            console.log('Fruta está vacío')
+            return
+        }
+
+        if(!descripcion.trim()){    
+            console.log('descripcion está vacío')
+            return
+        }
+
+        console.log('procesando datos...' + fruta + descripcion)
+
+        e.target.reset()
+        setFruta('')
+        setDescripcion('')
+
+    }
+
     return (
         <div>
             <h2>Formulario</h2>
-            <form>
+            <form onSubmit={ guardarDatos }>
                 <input 
                     type="text"
                     placeholder="Ingrese fruta"
@@ -21,7 +41,7 @@ const Formulario = () => {
                     className="form-control mb-2"
                     onChange={ e => setDescripcion(e.target.value) }
                 />
-                <button className="btn btn-primary btn-block">Agregar</button>
+                <button className="btn btn-primary btn-block" type="submit">Agregar</button>
             </form>
         </div>
     )
