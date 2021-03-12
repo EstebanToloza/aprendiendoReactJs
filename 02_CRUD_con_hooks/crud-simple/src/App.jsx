0e1475054f2df5c2fea7c9f8a4e291ad3e33517a
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-03-12 18:26:00 -0300

    Maquetado de CRUD basico con bootstrap. Formulario y generador de IDs con libreria shortid

---
+++ 02_CRUD_con_hooks/crud-simple/src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import shortid from 'shortid'
+
+
+function App() {
+
+  const [tarea, setTarea] = React.useState('')
+  const [tareas, setTareas] = React.useState([])
+
+  const agregarTarea = e => {
+    e.preventDefault()
+    if(!tarea.trim()){
+      console.log('Elemento vacio')
+      return
+    }
+    console.log(tarea)
+
+    setTareas([
+      ...tareas,
+      {id: shortid.generate(), nombreTarea:tarea}
+    ])
+
+    setTarea('')
+  }
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">CRUD Simple</h1>
+      <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de tareas</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <span className="lead">Nombre de la tarea</span>
+              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+              <button className="btn btn-warning btn-sm float-right mx-2">Editar</button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+          <form onSubmit={agregarTarea}>
+            <input 
+              type="text" 
+              className="form-control mb-2"
+              placeholder="Ingrese tarea"
+              onChange={ e => setTarea(e.target.value) }
+              value={tarea}
+            />
+            <button className="btb btn-dark btn-block" type="submit">Agregar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
