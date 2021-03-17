4ec9c8d9feba8e86534c1114a63208b3f7a25698
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-03-17 16:20:19 -0300

    Router Hook en React

---
+++ 04_Router/router/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import Contacto from './components/Contacto';
+import Inicio from './components/Inicio';
+import Nosotros from './components/Nosotros';
+
+function App() {
+  return (
+    <Router>
+    <div className="container">
+      <div className="btn-group mt-5">
+        <Link to="/" className="btn btn-dark">
+          Inicio
+        </Link>
+        <Link to="/nosotros" className="btn btn-dark">
+          Nosotros
+        </Link>
+        <NavLink to="/contacto" className="btn btn-dark" activeClassName="active">
+          Contacto
+        </NavLink>
+      </div>
+      <hr />
+      <Switch>
+      {/* <Route path="/" exact> //Se debe organizar las rutas de lo más específico a lo más general o usar 'exact'
+          Esta es la página de Inicio
+        </Route> */}
+        <Route path="/contacto">
+          <Contacto />
+        </Route>
+        <Route path="/nosotros">
+          <Nosotros />
+        </Route>
+        <Route path="/">
+          <Inicio />
+        </Route>
+      </Switch>
+      
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ 04_Router/router/src/components/Nosotros.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nosotros = () => {
+    return (
+        <div>
+            <h1>Nosotros</h1>
+        </div>
+    )
+}
+
+export default Nosotros
