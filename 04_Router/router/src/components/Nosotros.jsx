cd35df1232d4568c7a04c8765674436eea4a53e5
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-03-22 10:24:27 -0300

    Uso del hook useEffect

--- 04_Router/router/src/components/Nosotros.jsx
+++ 04_Router/router/src/components/Nosotros.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
 const Nosotros = () => {
+
+    
+
+    const [equipo, setEquipo] = React.useState([])
+
+    React.useEffect(() => {
+        console.log('useEffect')
+        obtenerDatos()
+    }, [])
+
+    const obtenerDatos = async () => {
+        const data = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await data.json()
+        //console.log(users)
+        setEquipo(users)
+    }
+
     return (
         <div>
             <h1>Nosotros</h1>
+            <ul>
+                {
+                    equipo.map(item => (
+                        <li key={item.id}>{item.name} - {item.email}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
