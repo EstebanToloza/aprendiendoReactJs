fd5afc0826e8f71e0bc2f58364cfe3e6019def8b
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-03-22 20:32:20 -0300

    Importacion de ant, font awesome y gogle fonts. Primeros componentes de header

---
+++ 06_react_ant_web/src/components/common/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import App from '../../App';
+import { Menu } from 'antd';
+
+
+function AppHeader(){
+    return(
+        <div className="container-fluid">
+            <div className="header">
+            <div className="logo">
+                <i class="fas fa-bolt"></i>
+                <a href="#">Tech</a>
+            </div>
+                <Menu mode="horizontal" defaultSelectedKeys={['home']}>
+                    <Menu.Item key="home">Home</Menu.Item>
+                    <Menu.Item key="about">About</Menu.Item>
+                    <Menu.Item key="features">Features</Menu.Item>
+                    <Menu.Item key="howiworks">How It Works</Menu.Item>
+                    <Menu.Item key="faq">FAQ</Menu.Item>
+                    <Menu.Item key="pricing">Pricing</Menu.Item>
+                    <Menu.Item key="contact">Contact</Menu.Item>
+                </Menu>
+            </div>
+        </div>
+    );
+}
+
+export default AppHeader;
---
+++ 06_react_ant_web/src/views/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppHero from '../components/home/hero'
+
+function AppHome() {
+    return (
+        <div className="main">
+            <AppHero />
+        </div>
+    )
+}
+
+export default AppHome;
