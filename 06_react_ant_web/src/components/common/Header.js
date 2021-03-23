4b3f998372449e2cd4f5cd796a2d0fb48e79972d
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-03-22 21:19:19 -0300

    Seccion about us terminada

--- 06_react_ant_web/src/components/common/Header.js
+++ 06_react_ant_web/src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import App from '../../App';
-import { Menu } from 'antd';
+/* import App from '../../App';
+ */import { Menu } from 'antd';
 
 
 function AppHeader(){
--- 06_react_ant_web/src/views/home.js
+++ 06_react_ant_web/src/views/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AppHero from '../components/home/hero'
+import AppAbout from '../components/home/about'
 
 function AppHome() {
     return (
         <div className="main">
             <AppHero />
+            <AppAbout />
         </div>
     )
 }
