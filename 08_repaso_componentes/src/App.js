40ad555d9736a9414d7e1d4416e6ba15800231af
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-07-18 23:14:57 -0300

    .

---
+++ 08_repaso_componentes/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Blog from './components/Blog';
+
+function App() {
+  return (
+    <div className="App">
+      <Blog />
+    </div>
+  );
+}
+
+export default App;
---
+++ 08_repaso_componentes/src/components/Blog/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <section>
+            <h1>Titulo</h1>
+            <p>dansudnas nduansduasnudas dansudnausdas</p>
+        </section>
+    )
+}
+
+export default Blog
