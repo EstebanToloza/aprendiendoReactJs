c400f35749a55b8308c8fbd855362671c3452062
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-07-19 23:54:57 -0300

    events

--- 08_repaso_componentes/src/App.js
+++ 08_repaso_componentes/src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import Blog from './components/Blog';
 
 function App() {
-  function clickMe(){
+  function handleClick() {
     console.log('clickeaste!')
   }
-  
+
+  function handleChange(e) {
+    const { value, name } = e.target
+    console.log(value, name)
+  }
+
   return (
     <div className="App">
       <Blog />
-      <button onClick={clickMe}>Click me!</button>
+      <button onClick={handleClick}>Click me!</button>
+      <label>nombre</label>
+      <input type="text" name="name" onChange={handleChange} />
+      <label>apellido</label>
+      <input type="text" name="surname" onChange={handleChange} />
+
     </div>
   );
 }
