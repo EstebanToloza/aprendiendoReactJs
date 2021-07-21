2034f3581ebb514868b70742130166915b0e3855
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-07-20 23:53:07 -0300

    Hook useState

--- 08_repaso_componentes/src/App.js
+++ 08_repaso_componentes/src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import Blog from './components/Blog';
+import { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState("")
+  const [counter, setCounter] = useState(0)
+
   function handleClick() {
-    console.log('clickeaste!')
+    setMessage('Soy un mensaje actualizado')
   }
 
   function handleChange(e) {
     const { value, name } = e.target
-    console.log(value, name)
+    setMessage(value)
+  }
+
+  function handleCounter() {
+    setCounter((prevCounter) => prevCounter + 1)
   }
 
   return (
     <div className="App">
+      <span>El valor del estado message es: {message}</span>
+      <span>El valor del contador es: {counter}</span>
+      <button onClick={handleCounter}>+1</button>
       <Blog />
       <button onClick={handleClick}>Click me!</button>
-      <label>nombre</label>
+      <label>Mensaje</label>
       <input type="text" name="name" onChange={handleChange} />
-      <label>apellido</label>
-      <input type="text" name="surname" onChange={handleChange} />
-
     </div>
   );
 }
