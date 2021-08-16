f42f929a31b58fff2503d1a1865fc4936e83d0fc
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-08-16 20:42:06 -0300

    hook useEffect

---
+++ 12_hook_usestate/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData () {
+      const getData = await fetch('https://api.github.com/users/EstebanToloza');
+      const dataToJson = await getData.json()
+
+      setUser(dataToJson);
+  }
+
+  return (
+    <div className="App">
+      <div>{user.login}</div>
+      <div>{user.type}</div>
+      <div>{user.public_repos}</div>
+    </div>
+  );
+}
+
+export default App;
