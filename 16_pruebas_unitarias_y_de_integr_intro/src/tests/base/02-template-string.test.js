d2f8543e411c99dc68b9330068517f63597af696
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-08-22 23:24:53 -0300

    tests con jest library

---
+++ 16_pruebas_unitarias_y_de_integr_intro/src/tests/base/02-template-string.test.js
@@ -0,0 +1,26 @@
+//import '@testing-library/jest-dom';
+
+import { getSaludo } from '../../base/02-template-string'
+
+describe('Pruebas en 02-template-string', () => {
+    test('getSaludo debe retornar hola + nombre', () => {
+        
+        const nombre = 'Esteban';
+
+        const saludo = getSaludo(nombre);
+
+        expect( saludo ).toBe('Hola ' + nombre); 
+    })    
+
+    test('getSaludo debe retornar hola + Carlos si no hay argumento', () => {
+        
+        const saludo = getSaludo();
+
+        expect( saludo ).toBe('Hola Carlos'); 
+    })    
+});
+
+// getSaludo debe retornar 'Hola Carlos' si no hay argumento en nombre
+
+    
+
