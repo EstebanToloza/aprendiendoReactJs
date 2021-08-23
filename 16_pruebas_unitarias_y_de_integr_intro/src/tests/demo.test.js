a21cffc19c56db07169325df8165de7f2312a0c2
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-08-22 23:03:25 -0300

    test con jest

--- 16_pruebas_unitarias_y_de_integr_intro/src/tests/demo.test.js
+++ 16_pruebas_unitarias_y_de_integr_intro/src/tests/demo.test.js
@@ -1,7 +1,15 @@
-test('is true ', () => {
-    const isActive = true;
+describe ('Pruebas en el archivo demo.test.js', () => {
+    test('Deben de ser iguales los string', () => {
+        const isActive = true;
+    
+        //1. Inicialización
+        const mensaje = 'Hola mundo';
+    
+        //2. Estímulo
+        const mensaje2 = `Hola mundo`;
+    
+        //3. Observar el comportamiento
+        expect( mensaje ).toBe( mensaje2 )
+    })    
+});
 
-    if (!isActive) {
-        throw new Error ('No está activo')
-    }
-})
