58f985bcf98496eb13e129a3ead3af6d4086947c
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-08-26 23:15:49 -0300

    inicializando proyecto

---
+++ 17_buscador_gif_app/src/Components/GifExpertApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GifExpertApp = () => {
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr />
+        </>
+    )
+}
+
+export default GifExpertApp
