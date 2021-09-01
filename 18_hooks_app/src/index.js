deec0446131b414a07302876afb6a3891b3ecc65
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-08-31 23:18:41 -0300

    manejo del useState con state tipo objeto, uso del operador de propagación

---
+++ 18_hooks_app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CounterApp from './components/01-useState/CounterApp';
+//import HookApp from './HookApp';
+
+
+
+ReactDOM.render(
+    <CounterApp />,  
+  document.getElementById('root')
+);
+
