81056e414914366094492c3e50173712bd6854fc
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-09-04 12:29:23 -0300

    form with custom hook

--- 18_hooks_app/src/index.js
+++ 18_hooks_app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SimpleForm from './components/02-useEffect/SimpleForm';
+import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+//import SimpleForm from './components/02-useEffect/SimpleForm';
 //import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook'
 //import CounterApp from './components/01-useState/CounterApp';
 //import HookApp from './HookApp';
@@ -9,7 +10,7 @@ import SimpleForm from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
   
-    <SimpleForm />,  
+    <FormWithCustomHook />,  
 
   document.getElementById('root')
 );
