927e5aaea765a7a90e0bff906d160b7bef7e66c5
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-09-04 12:00:59 -0300

    useEffect, eventos, precauciones

--- 18_hooks_app/src/components/02-useEffect/Message.js
+++ 18_hooks_app/src/components/02-useEffect/Message.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 
 const Message = () => {
 
+    const [coords, setCoords] = useState({ x: 0, y:0 })
+    const { x, y } = coords
+
     useEffect(() => {
-        console.log("Componente montado")
-        
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y };
+
+            setCoords( coords )
+            console.log(coords)
+            //console.log(":D")
+        }
+        window.addEventListener('mousemove', mouseMove);
+        //console.log("Componente montado")
+
         return () => {
-            console.log("Componente desmontado")
+            window.removeEventListener('mousemove', mouseMove)
         }
     }, [])
 
     return (
         <>
             <h3>Eres genial</h3>
+            <hr />
+            <p>
+                x:{ x }
+                y:{ y }
+            </p>
         </>
     )
 }
