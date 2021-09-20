765e8e83f6d0978436ec4a3197da0d79b8f2bc41
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-09-19 23:26:34 -0300

    Planteando primeros archivos. Creando primer router

---
+++ 21_spa_heroes_app/src/components/dc/DcScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DcScreen = () => {
+    return (
+        <div>
+            <h1>DcScreen</h1>
+        </div>
+    )
+}
+
+export default DcScreen
---
+++ 21_spa_heroes_app/src/routers/AppRouter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+import LoginScreen from '../components/login/LoginScreen';
+import Marvel from '../components/marvel/Marvel';
+import { Navbar } from '../components/ui/Navbar';
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/login" component={LoginScreen} />
+                    <Route exact path="/" component={Marvel} />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
+export default AppRouter
