e10ab6702aaf7a8e07efd9cda3b25b48ecbc7d3e
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-09-20 23:17:30 -0300

    useHistory, hook

--- 21_spa_heroes_app/src/components/login/LoginScreen.js
+++ 21_spa_heroes_app/src/components/login/LoginScreen.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
 
 const LoginScreen = () => {
+
+    let history = useHistory();
+
+    const handleLogin = () => {
+        //history.push('/')
+        history.replace('/')
+    }
+
     return (
-        <div>
+        <div className="container mt-5">
             <h1>LoginScreen</h1>
+            <hr />
+
+            <button
+                className="btn btn-primary"
+                onClick={handleLogin}
+            >
+                Login
+            </button>
         </div>
     )
 }
