49ec6f0ef241474b21d38d03fa54cad49a3c7d50
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-09-23 23:50:01 -0300

    component hero screen

--- 21_spa_heroes_app/src/components/heroes/HeroScreen.js
+++ 21_spa_heroes_app/src/components/heroes/HeroScreen.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import { getHeroesById } from '../selectors/getHeroesById';
+//import foto from '../../../public/assets/heroes/'
 
-const HeroScreen = () => {
+const HeroScreen = ({history}) => {
+
+    const {heroeId} = useParams();
+    const hero = getHeroesById(heroeId);
+
+    if (!hero) {
+        return <Redirect to="/" />
+    }
+
+    const handleReturn = () => {
+        if(history.length <=2) {
+            history.push('/');
+        } else {
+            history.goBack()
+        }
+    }
+
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero;
+
     return (
-        <div>
-            <h1>HeroScreen</h1>
+        <div className="row mt-5">
+            <div className="col-4"> 
+                <img src={`/assets/heroes/${heroeId}.jpg`} className="img-thumbnail" alt="hero" />
+            </div>
+            <div className="col-8">
+                <h3>{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-groupt-item">Alter ego: {alter_ego}</li>
+                    <li className="list-groupt-item">Publisher: {publisher}</li>
+                    <li className="list-groupt-item">First appearence: {first_appearance}</li>
+                    <li className="list-groupt-item">Characters: {characters}</li>
+                </ul>
+                <br />
+                <button className="btn btn-outline-info" onClick={handleReturn}>
+                    Return
+                </button>
+            </div>
         </div>
     )
 }
--- 21_spa_heroes_app/src/routers/DashboardRoutes.js
+++ 21_spa_heroes_app/src/routers/DashboardRoutes.js
@@ -13,7 +13,7 @@ const DashboardRoutes = () => {
             <div className="container mt-2">
                 <Switch>
                     <Route exact path="/marvel" component={MarvelScreen} />
-                    <Route exact path="/marvel/:heroeId" component={HeroScreen} />
+                    <Route exact path="/hero/:heroeId" component={HeroScreen} />
                     <Route exact path="/dc/" component={DcScreen} />
 
                     <Redirect to="/marvel" />
