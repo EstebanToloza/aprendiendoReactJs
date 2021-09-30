522c3dcb1b75b287278c4908fa91317eeaab922c
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-09-29 23:06:01 -0300

    authContext, created

--- 21_spa_heroes_app/src/HeroesApp.js
+++ 21_spa_heroes_app/src/HeroesApp.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from './auth/AuthContext'
+import { authReducer } from './auth/authReducer'
 import AppRouter from './routers/AppRouter'
 
+const init = () => {
+    return JSON.parse(localStorage.getItem('user')) || {
+        logged: false,
+    }
+}
+
 const HeroesApp = () => {
+
+    const [user, dispatch] = useReducer(authReducer, {}, init)
+
     return (
-        <AppRouter />
+
+        <AuthContext.Provider value={{ user, dispatch }}>
+            <AppRouter />
+        </AuthContext.Provider>
     )
 }
 
