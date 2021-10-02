04aac2aedcde6fd21ec087afa932931652edbbc0
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-10-02 12:00:39 -0300

    private route when user logout

--- 21_spa_heroes_app/src/routers/AppRouter.js
+++ 21_spa_heroes_app/src/routers/AppRouter.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
   } from "react-router-dom";
+import { AuthContext } from '../auth/AuthContext';
 import LoginScreen from '../components/login/LoginScreen';
 import { Navbar } from '../components/ui/Navbar';
 import DashboardRoutes from './DashboardRoutes';
+import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => {
+
+    const { user } = useContext( AuthContext )
+
     return (
         <Router>
             <div>
                 <Switch>
                     <Route exact path="/login" component={LoginScreen} />
-                    <Route path="/" component={DashboardRoutes} />
+                    <PrivateRoute 
+                        path="/" 
+                        component={DashboardRoutes} 
+                        isAuthenticated={ user.logged }
+                    />
                 </Switch>
             </div>
         </Router>
