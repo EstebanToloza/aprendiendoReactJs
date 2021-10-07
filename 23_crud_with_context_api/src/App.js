8e6883127e393c29dbf062df8a09ec2eb1de6cc6
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-10-06 23:34:16 -0300

    Starting project, first styles, imports and routes

---
+++ 23_crud_with_context_api/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Heading from './components/Heading';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+
+function App() {
+  return (
+    <>
+      <div className="h-screen text-white text-center p-10">
+        <div className="container mx-auto h-full">
+          <Heading />
+          <Switch>
+            <Route exact path='/' component={TaskList} />
+            <Route exact path='/add' component={TaskForm} />
+          </Switch>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
